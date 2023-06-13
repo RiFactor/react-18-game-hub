150263629d259cc187a3d6a7a17b65cb1c5c1da1
Ri <ri@3sidedcube.com>
2023-06-13 15:13:06 +0100

    Creating a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button colorScheme="green">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // mobile devices
+        lg: `"nav nav" "aside main"`, // 1024px // Question -- why are there two columns
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="lavender">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
