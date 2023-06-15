75c959ea13fa50887ef32890426d821c40830593
Ri <ri@3sidedcube.com>
2023-06-15 12:17:55 +0100

    Improve UX with Loading Skeletons

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 export const GameCardSkeleton = () => {
   return (
     <Card width="200px" borderRadius={10} overflow="hidden">
+      {/* Question -- why is the height on the skeleton */}
       <Skeleton height="300px" />
       <CardBody>
         <SkeletonText />
