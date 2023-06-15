6dcaa9fbf4657dd275852c38c2a605a549221e30
Ri <ri@3sidedcube.com>
2023-06-15 09:51:44 +0100

    Displaying Platform Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+
+interface Props {
+  // Question -- review this w/ useGames.tsx
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // define an object as a map, use keys that represent the slug (less likely to change than name) of each platform
+  // index signature when wrapped in []
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
