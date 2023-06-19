abe820c61aa58743a070d6cf20940980611140cf
Ri <ri@3sidedcube.com>
2023-06-19 15:45:25 +0100

    Use env var w/ Owen

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-// const API_KEY = process.env.REACT_APP_API_KEY // Online -- how do I store an environment variable: https://vitejs.dev/guide/env-and-mode.html
-
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_API_KEY, // included in query string of each HTTP request
+    // included in query string of each HTTP request:
+    key: import.meta.env.VITE_API_KEY, // https://vitejs.dev/guide/env-and-mode.html
   },
 });
