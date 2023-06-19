0bd6cfaed6d352ec99d903a853c459729a3a3d82
Ri <ri@3sidedcube.com>
2023-06-19 17:43:52 +0100

    Provide placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import ImagePlaceholder from "../assets/image-placeholder.webp";
+
 const GetCroppedImageUrl = (url: string) => {
-  if (!url) return; // !url instead of url === null (Question -- better catch all?)
+  if (!url) return ImagePlaceholder; // !url instead of url === null (Question -- better catch all?)
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const newUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
