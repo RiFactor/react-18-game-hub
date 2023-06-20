5d36c21a0bd3e74b2ebcb6a8cc11727844c5498b
Ri <ri@3sidedcube.com>
2023-06-20 18:33:31 +0100

    can't use API Key for prod

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     // included in query string of each HTTP request:
-    key: import.meta.env.VITE_API_KEY, // https://vitejs.dev/guide/env-and-mode.html
-    // key: "35ff2db8a79e4a2e856db3b723d0b66b", // https://vitejs.dev/guide/env-and-mode.html
+    // key: import.meta.env.VITE_API_KEY, // https://vitejs.dev/guide/env-and-mode.html
+    key: "35ff2db8a79e4a2e856db3b723d0b66b", // https://vitejs.dev/guide/env-and-mode.html
   },
 });
