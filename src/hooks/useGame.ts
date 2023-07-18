1667d5d9b14a79e4f22602e6c840450ab05c9dd3
Ri <ri@3sidedcube.com>
2023-07-18 14:44:26 +0100

    Exercise: Build Expandable Text

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import { Game } from "../entities/Game";
 
-const apiClient = new APIClient<Game>("/games"); // code smell, relying on useGames
+const apiClient = new APIClient<Game>("/games");
 
 const useGame = (slug: string) =>
   useQuery({
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
+  const [showText, setShowText] = useState(false);
   // console.log(slug);
   const { data: game, isLoading, error } = useGame(slug!); // slug || ""
   // as { data: Game; isLoading: boolean; error: any };
@@ -13,10 +15,41 @@ const GameDetailPage = () => {
 
   if (error || !game) throw error; // stuck on infinite load when slug doesn't exist
 
+  const handleHideText = (text: string) => {
+    return (
+      <>
+        {text.slice(0, 300)}
+        <button
+          onClick={() => {
+            console.log("show");
+            setShowText(!showText);
+          }}
+        >
+          More
+        </button>
+      </>
+    );
+  };
+
+  const handleShowText = (text: string) => {
+    return (
+      <>
+        {text}
+        <button onClick={() => setShowText(!showText)}>Less</button>
+      </>
+    );
+  };
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <Text>
+        {game.description_raw.length > 300
+          ? showText
+            ? handleShowText(game.description_raw)
+            : handleHideText(game.description_raw)
+          : game.description_raw.length}
+      </Text>
     </>
   );
 };
