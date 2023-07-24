fd95863e58a077f808f83d0f8c2038ccdea64c6f
Ri <ri@3sidedcube.com>
2023-07-24 12:07:25 +0100

    Exercise: Building Game Screenshots - media query for columns

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -1,5 +1,6 @@
+import { css } from "@emotion/react";
 import useScreenshots from "../hooks/useScreenshots";
-import { Image } from "@chakra-ui/react";
+import { Image, SimpleGrid } from "@chakra-ui/react";
 
 interface IProps {
   gameId: number;
@@ -14,14 +15,27 @@ const GameScreenshots = ({ gameId }: IProps) => {
 
   const array = data?.results;
 
-  return array
-    ? array.map((image: any) => <Image key={image.id} src={image.image} />)
-    : null;
+  // ChatGPT
+  const customGridStyles = css`
+    @media screen and (max-width: 767px) {
+      grid-template-columns: 1fr;
+    }
 
-  // return null;
-  // return <div>hi</div>;
+    /* Larger screens: 2 columns */
+    @media screen and (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+    }
+  `;
 
-  // return <Image src={data?.results[0].image} />;
+  return array ? (
+    // <div className="grid-container bg">
+    <SimpleGrid columns={2} spacing={10} css={customGridStyles}>
+      {array.map((image: any) => (
+        <Image key={image.id} src={image.image} />
+      ))}
+    </SimpleGrid>
+  ) : // </div>
+  null;
 };
 
 export default GameScreenshots;
