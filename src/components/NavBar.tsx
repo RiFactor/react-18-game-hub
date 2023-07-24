288f73708e6dfdd43976fdf289692dd51d9bdfc4
Ri <ri@3sidedcube.com>
2023-07-24 14:09:48 +0100

    Exercise: Fix the Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,18 @@ import { HStack, Image } from "@chakra-ui/react";
 import Logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     // justifyContent="space-between" no longer needed w/ 3 elements
     <HStack padding="0.625rem" spacing={2}>
-      <Image src={Logo} boxSize="60px" />
-      <SearchInput />
+      <Link to="/">
+        <Image src={Logo} boxSize="60px" />
+      </Link>
+      <Link to="/">
+        <SearchInput />
+      </Link>
       <ColorModeSwitch />
     </HStack>
   );
